3a8c19ed047e045d74f1909f9fc5afe4a3ec2b12
Ana Silva <ananunesdasilva@gmail.com>
2018-03-27 22:11:46 +0100

    Added associations and validations to models and created routes for entries

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,7 @@
 class Entry < ApplicationRecord
+  belongs_to :user
+  validates_presence_of [:entry, :category]
+
+  enum category: [:entertainment, :health_care, :utilities,
+                  :personal_care, :rent, :groceries, :home_supplies, :other]
 end
