0f8f01f687a7c2c521eea6756b6e96ae95288341
Ana Nunes da Silva <ananunesdasilva@gmail.com>
2018-06-01 18:09:09 +0100

    Completed the CRUD for Entry

--- app/controllers/api/v1/entries_controller.rb
+++ app/controllers/api/v1/entries_controller.rb
@@ -5,6 +5,29 @@ module Api
         @entries = Entry.all
         render json: @entries
       end
+
+      def show
+        @entry = Entry.find(params[:id])
+        render json: @entry
+      end
+
+      def create
+        Entry.create(entry_params)
+      end
+
+      def update
+        Entry.find(params[:id]).update_attributes(entry_params)
+      end
+
+      def destroy
+        Entry.find(params[:id]).destroy
+      end
+
+      private
+
+      def entry_params
+        params.permit(:amount, :description, :category, :date, :entry_type, :user_id)
+      end
     end
   end
 end
