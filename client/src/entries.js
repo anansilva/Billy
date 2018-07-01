cc6671b15745c133c40e026b91ccc4d97f422a05
Ana Silva <ananunesdasilva@gmail.com>
2018-07-01 19:35:26 +0100

    Styled entries component

--- client/src/entries.js
+++ client/src/entries.js
@@ -1,5 +1,17 @@
 import React from 'react';
 import _ from 'lodash';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBolt } from '@fortawesome/free-solid-svg-icons'
+import { faBriefcaseMedical } from '@fortawesome/free-solid-svg-icons'
+
+// const iconStyle = {
+//   borderRadius: '50%',
+//   backgroundColor: 'orange',
+//   color: 'white',
+//   height: '40px',
+//   width: '40px',
+//   padding: '8px'
+// };
 
 function Entries(props) {
   return (
@@ -8,14 +20,15 @@ function Entries(props) {
         <div className="container entries-component" key={entry.id}>
           <div className="row entry-date">{entry.date}</div>
           <div className="row">
-            <div className="col-sm">
-              {entry.category}
+            <div className="col-sm entries-description">
+                <button type="button" className="btn btn-danger btn-circle">
+                  {entry.category === 'utilities' && <FontAwesomeIcon icon={faBolt}/>}
+                  {entry.category === 'health_care' && <FontAwesomeIcon icon={faBriefcaseMedical}/>}
+                </button>
+              <span className="entry-description">{entry.description}</span>
             </div>
-            <div className="col-sm">
-              {entry.description}
-            </div>
-            <div className="col-sm">
-              {entry.amount}
+            <div className="col-sm entries-amount">
+              {entry.amount}€
             </div>
           </div>
         </div>
